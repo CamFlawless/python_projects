72df2622f89a5a4f4fd5504da717979b11c9b79a
Cameron Collins <camjcollins@gmail.com>
2016-09-30 11:51:41 -0400

    Update menu.py

--- baby-steps/menu.py
+++ baby-steps/menu.py
@@ -23,14 +23,17 @@
   # an order. Once the user has entered an order, print out how many of each item have been 
   # ordered, as well as the total price. If an item was not ordered at all, then it should not show up.
   
- 
-
-
+  # Add in a way to track the total number of transactions completed during the program's runtime 
+  # as well as the total sales dollars that should have been collected by the user 
+    # - As though someone was going to balance and check their till after finishing for the night 
+	# - Similar to how we kept track of the game history in the higher/lower game 	
+  
  
 import time   
 
 
 def _main_():
+	# a welcome prompt that will only display on the initilization of the program 
 	print ('''
 			Welcome to the easy-menu genie. I was designed to make your life
 			as a waiter as easy as can be. Simply enter the numbers of the items
@@ -55,8 +58,7 @@ def _main_():
 def _CustomerOrder_():	
 
 	# Let's make use of Python's built in dictionary data type 
-	# I have to imagine that there is a way to combine these into a single var 
-	# Learn how to do it and make it work!  
+	# Is it possible and/or beneficial to combine the two dicts below into one? 
 	menu_order = {
 		1: 'Chicken Strips',
 		2: 'French Fries', 
@@ -82,42 +84,63 @@ def _CustomerOrder_():
 		}
 
 	customer_order = input("Enter the customer's order number(s)	> ")
+	# sanity check; did the user input a valid response
 	try:
 		int(customer_order)
+	# in the event they did not, start the order over 
 	except ValueError:
 		print ("Are you sure you entered numbers? Let's try that again!")
-		_CustomerOrder_()
+		customer_order = _RedoOrder_()
 		
+	# creating empty list that I will append with the order info
+	# one for the items ordered and one for the corresponding prices 
 	order_prices =  []
 	order_items = []
 	for x in customer_order:
 		order_prices.append(menu_prices[menu_order[int(x)]])
 		order_items.append(menu_order[int(x)])
-		
+	
+	# print out the entered order to the user to ensure accuracy 
 	for x in menu_prices:
 		item_count = order_items.count(x)
 		if item_count > 0:
-			print ('Item: %s | Quantity: %s | Item Price: %s | Extended Price: %s' % (x,str(item_count), str(menu_prices[x]), str(menu_prices[x] * item_count)))
-	
-	# Should I create a new function to handle this logic?
-	# Want to avoid iterating endlessly 
-	# Would also help to add better conditional commentary/print statements 
-	response = input(("Does the list above match the customer's order?	(y/n)	>  	"))
-	if response != "n" and response  != "y":
-		print ("Please enter a valid response - y for yes or n for no")
-		response = input(("Does the list above match the customer's order?	(y/n)	>  	"))
-	elif response == "n":
-		_CustomerOrder_()	
-	
-	item_counts = []
-	for x in order_items:
-		item_counts.append(order_items.count(x))
+			print ('Item: %s | Quantity: %s | Item Price: %s | Extended Price: %s'
+			% (x,str(item_count), str(menu_prices[x]), str(menu_prices[x] * item_count)))
 			
+	order_status = False 		
+	while order_status != True:
+		order_status = _SanityCheck_()
+		
+
+	# sum the list containing the prices to provide the user with the order total price 
 	order_total = sum(order_prices)	
 	print ("The customer's order total is $%s" % (order_total))
 	
+	# call on the _Again_ function to ask the user if they would like to begin another order 
 	_Again_() 
-	
+		
+def _SanityCheck_():
+	response = input("Does the order above match your customer's order?" +
+					" (y/n) >	")
+	response = response.strip()
+	if response != "y" and response != "n":
+		print ("Please enter a valid response - y for yes or n for no")
+		_SanityCheck_()
+	elif response == "y":
+		return True
+	elif response == "n":
+		new_order = _RedoOrder_()
+		return new_order 
+		
+def _RedoOrder_():
+	customer_order = input("Enter the customer's order number(s)	> ")
+	try:
+		int(customer_order)
+	except ValueError:
+		print ("Are you sure you entered a number? Let's try that again!")
+		_RedoOrder_()
+	return customer_order 
+
 def _Again_():
 	response = input("Would you like to begin another order? (y/n)	>	")
 	if response != "y" and response != "n":
@@ -130,8 +153,5 @@ def _Again_():
 		print ("Shutting Down...")
 		time.sleep(0.5)
 		exit 
-				
-
-
-		
+	
 _main_()
