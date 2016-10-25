f7fbc38e1ba49e859b5c3e674bd030309f12c0b9
Cameron Collins <camjcollins@gmail.com>
2016-10-25 17:00:06 -0400

    Update fitness.py

--- baby-steps/fitness.py
+++ baby-steps/fitness.py
@@ -13,8 +13,6 @@ Meal-parts are within <tr class="meal_header">
 Item entries are within <td class="first alt"> 
 
 
-
-
 '''
 
 
@@ -27,11 +25,19 @@ s = urllib.request.urlopen(my_url)
 
 soup = BeautifulSoup(s,'html.parser')
 
-tables = soup.findAll("table")
-
+tables = soup.findAll('table')
 entries = soup.findAll('td', {'class' : 'first alt'})
-meals = soup.findAll('tr', {'class' : 'meal_header'})
+nutrients = soup.findAll('td', {'class' : 'alt nutrient-column'})
 
-print(meals)
-		
+# empty dict to store each meal_part in (bfast, lunch, dinner, snack)
+meal_parts = {}
+for element in entries:
+    if 'href' not in str(element) and len(element.string.split()) == 1:
+        meal_parts[element.string] = []
+
+
+
+print(nutrients)
+
+
 
