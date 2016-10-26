e2709170c3a4fcf86c309cc6e8777bb3d16abd86
Cameron Collins <camjcollins@gmail.com>
2016-10-26 16:48:58 -0400

    Update fitness.py

--- baby-steps/fitness.py
+++ baby-steps/fitness.py
@@ -4,6 +4,12 @@
 # Once determined, write functions to go and retrieve each data point 
 # Write out to CSV (will surely have to do more clean up here) 
 
+# END OUTPUTS:
+# - Meal Parts
+# - Entries -- items eaten/consumer 
+# - Macros -- Carbs, Fat, Protein 
+# - Other Nutrients -- Calories, Sodium, Sugar 
+
 
 from bs4 import BeautifulSoup 
 import urllib.request 
@@ -22,6 +28,7 @@ s = urllib.request.urlopen(my_url)
 
 soup = BeautifulSoup(s,'html.parser')
 
+# Results set to be used; scraped HTML tags with relevant information 
 tables = soup.findAll('table')
 entries = soup.findAll('td', {'class' : 'first alt'})
 nutrients = soup.findAll('td', {'class' : 'alt nutrient-column'})
@@ -41,36 +48,30 @@ for element in other_nuts:
 
 other_nutrients_list = [tuple(other_nutrients[i:i+3]) for i in 
 					 range(0,len(other_nutrients),3)]
-print(other_nutrients_list)
 
-
-##PRINT DEBUGGING
-# print(tables)
-# print(entries)
-# print(nutrients)
-# print(other_nutrients_list)
-
-
+# include both the meal_part headers and food entries 
 diary_entries = []
 for element in entries:
     if 'href' not in str(element):
 	    diary_entries.append(element.string.strip())
-
+		
+# includes only the meal part headers 
 meal_parts = [] 
 for element in entries:
     if 'href' not in str(element) and len(element.string.split()) == 1:
         meal_parts.append(element.string)
 
+# includes only the food entries 
 food = [] 
 counter= 0 
 for element in entries:
     if 'href' not in str(element) and len(element.string.split()) > 1:
 	    counter = counter + 1 
 	    food.append(element.string.strip() + ' - ' +str(counter))
-	
+
+# includes the macro values in a single list 		
 macro_values = []
 for element in macroValues:
-	#print(str(element) + '\n\n\n')
     try:
 	    macro_values.append(int(element.string.strip()))
     except ValueError:
@@ -83,29 +84,26 @@ for element in macroPercent:
     except ValueError:
 	    macro_percentages.append(0)
 
+# creating a list of tuples for the macro values/percents 
+# since we know they are always reported in the same order: C, F, P
+# can then extract each tuple and connect it to the correspnding entry 		
+macro_value_list = [tuple(macro_values[i:i+3]) for i in range(0,len(macro_values),3)]
+macro_percent_list = [tuple(macro_percentages[i:i+3]) for i in 
+					  range(0,len(macro_percentages),3)]
+
 macros = ['Carbs', 'Fat', 'Protein']		
 macros_remaining = macro_values[len(macro_values)- 3:len(macro_values)]	
 for i in range(0, len(macros_remaining)):
     macros_remaining[i] = macros[i] + ' - ' + str(macros_remaining[i]) + 'g'
+
+del macro_value_list[len(macro_value_list)-1]
+
+print(macro_value_list)
 	
-
-##PRINT DEBUGGING 	
-# print((macro_values))
-# print((macro_percentages))
-# print(macros_remaining) 
-# print(food)
-
+	
 # SOURCE: 
 # http://stackoverflow.com/questions/15266593/how-to-group-list-items-into-tuple
 # Converting the macro data into a list of tuples, where each tuple provides 
 # the values for (Cabrs, Fat, Protein) 
-macro_value_list = [tuple(macro_values[i:i+3]) for i in range(0,len(macro_values),3)]
-macro_percent_list = [tuple(macro_percentages[i:i+3]) for i in 
-					  range(0,len(macro_percentages),3)]
+
 					  
-# print('Macro Values') 
-# print(macro_value_list) 
-# print('Macro Percentages') 
-# print(macro_percent_list) 
-# print('Macros Remaining')
-# print(macros_remaining) 
